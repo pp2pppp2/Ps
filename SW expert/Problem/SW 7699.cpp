18bff6bf19cbf2d3eb35251cd60db247880fce78
pp2pppp2 <37303200+pp2pppp2@users.noreply.github.com>
2019-09-06 09:26:57 +0900

    Update SW 7699.cpp

--- SW expert/Problem/SW 7699.cpp
+++ SW expert/Problem/SW 7699.cpp
@@ -1,26 +1,18 @@
 #include<iostream>
 #include<string.h>
-//#include <cstdio>
 #define _CRT_SECURE_NO_WARNINGS
 int visit[21][21];
 char land[21][21];
 bool landmark['Z' - 'A' + 1];
 int landmark_num;
-int R;
-int C;
-int Max;
-int limit;
-
+int R, C, Max, limit;
 
 using namespace std;
-
 void search(int x, int y)
 {
-	if (landmark[land[x][y] - 'A'])
-	{
+	if (landmark[land[x][y] - 'A'])	{
 		return;
 	}
-
 	landmark[land[x][y] - 'A'] = true;
 	landmark_num++;
 
@@ -28,24 +20,20 @@ void search(int x, int y)
 		Max = landmark_num;
 	if (Max == limit)
 		return;
-
+	
 	visit[x][y] = 1;
-
 	if (y != 0 && visit[x][y - 1] == 0) {
 		search(x, y - 1);
 	}
-
 	if (x != R - 1 && visit[x + 1][y] == 0) {
 		search(x + 1, y);
 	}
-
 	if (y != C - 1 && visit[x][y + 1] == 0) {
 		search(x, y + 1);
 	}
 	if (x != 0 && visit[x - 1][y] == 0) {
 		search(x - 1, y);
 	}
-
 	visit[x][y] = 0;
 	landmark_num--;
 	landmark[land[x][y] - 'A'] = false;
@@ -54,46 +42,27 @@ int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-
-
 	//freopen("input.txt", "r", stdin);
-
-
 	cin >> T;
 
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-
-		memset(landmark, 0, sizeof(landmark));
-		
-		
-		cin >> R;
-		cin >> C;
-
+		memset(landmark, 0, sizeof(landmark));			
+		cin >> R >> C;
 		landmark_num = 0;
-
 		limit = 0;
-		
-
-
-		for (int r = 0; r < R; r++)
-		{
-			for (int c = 0; c < C; c++)
-			{
+		for (int r = 0; r < R; r++) {
+			for (int c = 0; c < C; c++) {
 				cin >> land[r][c];
-				if (!landmark[land[r][c]-'A'])
-				{
+				if (!landmark[land[r][c]-'A'])	{
 					landmark[land[r][c]-'A'] = true;
 					limit++;
 				}
 			}
 		}
-		
 		memset(landmark, 0, sizeof(landmark));
-
 		Max = 0;
 		search(0, 0);
-
 		cout << "#" << test_case << " " << Max << '\n';
 	}
 	return 0;
